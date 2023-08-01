c37c4c4f689deb8fb00c34067265dbf3ec7be6e7
BeztDonkey <vanbang9710@gmail.com>
2023-08-01 22:02:23 +0700

    add obs-plugin-input-overlay.json

--- autoupdate.ps1
+++ autoupdate.ps1
@@ -2,16 +2,19 @@
 $checkver = "$env:SCOOP/apps/scoop/current/bin/checkver.ps1"
 $dir = 'E:\GitApps\scoop-apps\bucket'
 
-# & $checkver ./mpv-webm.json
+# & $checkver ./obs-plugin-input-overlay.json
 scoop config debug $true
-& $checkver -Dir $dir mpv-webm
-& $checkver -Dir $dir mpv-webm -ForceUpdate
+& $checkver -Dir $dir obs-plugin-input-overlay
+& $checkver -Dir $dir obs-plugin-input-overlay -ForceUpdate
+scoop install E:\GitApps\scoop-apps\bucket\obs-plugin-input-overlay.json
+scoop update obs-plugin-input-overlay -f
+scoop uninstall obs-plugin-input-overlay -p
+scoop update
 
 & $checkver -Dir $dir *
 & $checkver -Dir $dir * -Update
 & $checkver -Dir $dir * -ForceUpdate
 & $checkver -Dir $dir * -SkipUpdated
-& $checkver -Dir $dir mpv-webm -Version 4.6.2
-& $checkver -Dir $dir mpv-webm -Update -Version 4.6.2
-scoop update
+& $checkver -Dir $dir obs-plugin-input-overlay -Version 4.6.2
+& $checkver -Dir $dir obs-plugin-input-overlay -Update -Version 4.6.2
 scoop status
