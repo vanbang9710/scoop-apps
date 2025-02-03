3c50fbd983eccd4c3947ab8d18a515db8b8a5b37
BeztDonkey <vanbang9710@gmail.com>
2025-02-03 20:39:29 +0700

    Update mpv-webm installation and manifest for autoupdate

--- TestNewManifest.ps1
+++ TestNewManifest.ps1
@@ -1,12 +1,11 @@
 #1 Autoupdate workflow https://github.com/ScoopInstaller/Scoop/wiki/App-Manifest-Autoupdate
 $checkver = "$env:SCOOP/apps/scoop/current/bin/checkver.ps1"
-$dir = 'E:\GitApps\scoop-apps\bucket-reference'
-scoop config debug $true
-& $checkver -Dir $dir mpv
-& $checkver -Dir $dir mpv -ForceUpdate
-scoop install "$dir\mpv.json"
-scoop update mpv -f
-scoop uninstall mpv
+$dir = 'E:\GitApps\scoop-apps\bucket'
+& $checkver -Dir $dir mpv-webm
+& $checkver -Dir $dir mpv-webm -ForceUpdate
+scoop install "$dir\mpv-webm.json"
+scoop update mpv-webm -f
+scoop uninstall mpv-webm
 # git push
 scoop update
 
@@ -14,6 +13,6 @@ scoop update
 & $checkver -Dir $dir * -Update
 & $checkver -Dir $dir * -ForceUpdate
 & $checkver -Dir $dir * -SkipUpdated
-& $checkver -Dir $dir mpv -Version 0.37.0
-& $checkver -Dir $dir mpv -Update -Version 0.37.0
+& $checkver -Dir $dir mpv-webm -Version 0.37.0
+& $checkver -Dir $dir mpv-webm -Update -Version 0.37.0
 scoop status
