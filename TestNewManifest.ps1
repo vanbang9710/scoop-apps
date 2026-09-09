9c952c0519ff0d356782ca5a37288a5633a1d621
BeztDonkey <vanbang9710@gmail.com>
2026-09-09 17:16:03 +0700

    Squashed changes for 2026-09-09

--- TestNewManifest.ps1
+++ TestNewManifest.ps1
@@ -1,11 +1,11 @@
 #1 Autoupdate workflow https://github.com/ScoopInstaller/Scoop/wiki/App-Manifest-Autoupdate
 $checkver = "$env:SCOOP/apps/scoop/current/bin/checkver.ps1"
 $dir = 'E:\GitApps\scoop-apps\bucket'
-& $checkver -Dir $dir mpv-webm
-& $checkver -Dir $dir mpv-webm -ForceUpdate
-scoop install "$dir\mpv-webm.json"
-scoop update mpv-webm -f
-scoop uninstall mpv-webm
+& $checkver -Dir $dir aris
+& $checkver -Dir $dir aris -ForceUpdate
+scoop install "$dir\aris.json"
+scoop uninstall aris
+scoop update aris -f
 # git push
 scoop update
 
@@ -13,6 +13,6 @@ scoop update
 & $checkver -Dir $dir * -Update
 & $checkver -Dir $dir * -ForceUpdate
 & $checkver -Dir $dir * -SkipUpdated
-& $checkver -Dir $dir mpv-webm -Version 0.37.0
-& $checkver -Dir $dir mpv-webm -Update -Version 0.37.0
+& $checkver -Dir $dir aris -Version 0.37.0
+& $checkver -Dir $dir aris -Update -Version 0.37.0
 scoop status
