c6a881e33f5f046d457fe04eab0a713df0ee9e2c
BeztDonkey <vanbang9710@gmail.com>
2026-09-09 17:32:38 +0700

    Add qttabbar-indiff-np manifest and update autoupdate workflow

--- TestNewManifest.ps1
+++ TestNewManifest.ps1
@@ -1,11 +1,13 @@
 #1 Autoupdate workflow https://github.com/ScoopInstaller/Scoop/wiki/App-Manifest-Autoupdate
 $checkver = "$env:SCOOP/apps/scoop/current/bin/checkver.ps1"
 $dir = 'E:\GitApps\scoop-apps\bucket'
-& $checkver -Dir $dir aris
-& $checkver -Dir $dir aris -ForceUpdate
-scoop install "$dir\aris.json"
-scoop uninstall aris
-scoop update aris -f
+& $checkver -Dir $dir qttabbar-indiff-np
+& $checkver -Dir $dir qttabbar-indiff-np -ForceUpdate
+scoop install "$dir\qttabbar-indiff-np.json"
+gsudo scoop install "$dir\qttabbar-indiff-np.json"
+scoop uninstall qttabbar-indiff-np
+gsudo scoop uninstall qttabbar-indiff-np
+scoop update qttabbar-indiff-np -f
 # git push
 scoop update
 
@@ -13,6 +15,6 @@ scoop update
 & $checkver -Dir $dir * -Update
 & $checkver -Dir $dir * -ForceUpdate
 & $checkver -Dir $dir * -SkipUpdated
-& $checkver -Dir $dir aris -Version 0.37.0
-& $checkver -Dir $dir aris -Update -Version 0.37.0
+& $checkver -Dir $dir qttabbar-indiff-np -Version 0.37.0
+& $checkver -Dir $dir qttabbar-indiff-np -Update -Version 0.37.0
 scoop status
